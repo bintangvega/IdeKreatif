7e59e3457402f92b1ad34c4fbbbcc321474eb7b1
bintang-naisya123 <bintang.naisya17@smk.belajar.id>
2025-02-04 14:57:24 +0700

    posts.php

--- dashboard.php
+++ dashboard.php
@@ -1,3 +1,32 @@
 <?php
 include (".includes/header.php");
 $title = "Dashboard";
+// Menyertakan file untuk menampilkan notifikasi (jika ada)
+include '.includes/toast_notification.php';
+?>
+<div class="container-xxl flex-grow-1 container-p-y">
+    <!-- Card untuk menampilkan tabel postingan -->
+     <div class="card">
+        <!-- Tabel dengan baris yang dapat di hover -->
+         <div class="card">
+            <!-- Header Tabel -->
+             <div class="card-header d-flex justify-content-between alight-items-center">
+                <h4>Semua Potingan</h4>
+            </div>
+            <div class="card-body">
+                <!-- Tabel responsif -->
+                <div class="table-responsive text-nowrap">
+                    <table id="datatable" class="table table-hover">
+                        <thead>
+                            <tr class="text-center">
+                              <th width="50px">#</th>
+                              <th>Judul Post</th>
+                              <th>Penulis</th>  
+                              <th>Kategori</th>      
+                              <th width="150px">Pilihan</th>
+                            </tr>
+                        </thead>
+                        <tbody class="table-border-bottom-0">
+<!-- Menampilkan data dari tabel database -->
+ <?php
+  $index = 1; // Variabel untuk
--- posts.php
+++ posts.php
@@ -1,6 +1,6 @@
 <?php
 // Menyertakan header halaman
-include '.inlude/header.php';
+include '.include/header.php';
 ?>
 <div class="container-xxl flex-grow-1 container-p-y">
     <!-- Judul halaman -->
@@ -21,3 +21,37 @@ include '.inlude/header.php';
                         <label for="formFile" class="form-label">Unggah Gambar</label>
                         <input class="form-control" type="file" name="image" accept="image/*"/>
                     </div>
+                    <!-- Dropdown untuk memilih kategori -->
+                     <div class="mb-3">
+                     <lable for="category_id" class="form-label">Kategori</label>
+                     <select class="form-select" name="category_id" required>
+                        <!-- Mengambil data kategori dari database untuk mengisi opsi dropdown -->
+                         <option value="" selected desable> Pilih Aalah satu</option>
+                         <?php
+                         $query = "SELECT * FROM categories"; // Query ubtuk manembil data kategori
+                         $result = $conn->query($query); // Menjalankan query
+                         if ($result->num_rows > 0) { // Jika terdapat data kategori
+                            while ($row = $result->fetch_assoc()) { // Itelari setiap kategori
+                                echo ",option value='" . $row["category_id"] . "'>" . $row["category_name"] . "</option>";
+                            }
+                        }
+                        ?>
+                        </select>
+                    </div>
+                    <!-- Textarea untuk konten postingan -->
+                     <div class="mb-3">
+                     <label for="content" class="form-label">Konten</label>
+                     <textarea class="form-control" id="content" name="content" required></textarea>
+                    </div>
+                    <!-- Tombol submit -->
+                     <button type="submit" name="simpan" class="btn btn-primary">Simpan</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php
+// Menyertakan footer halaman
+include '.includes/footer.php';
+?>
